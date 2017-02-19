ab53efb132207712bbdf9865ec042e8f7626d736
MyMac <mymac@mymac-no-macbook-pro.local>
2017-02-20 00:36:18 +0800

    exercise1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var sum = 0
+for i in 0...7{
+    for j in 0...7{
+        sum = sum + i*j
+        
+    }
+}
+sum
