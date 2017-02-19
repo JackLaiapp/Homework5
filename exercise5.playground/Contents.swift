da67d61c6f751a83847b6e0ad1761765a000617e
MyMac <mymac@mymac-no-macbook-pro.local>
2017-02-20 01:26:39 +0800

    Exercise5

---
+++ exercise5.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func addToValue(startValue:Int, endValue:Int, multipleValue:Int) -> Int{
+    var sum = 0
+    
+    for i in startValue...endValue where i%multipleValue > 0 {
+        
+        sum = sum + i
+    }
+    return sum
+}
+addToValue(startValue: 3, endValue: 11, multipleValue: 5)
