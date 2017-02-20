f996df9b0a2c8b849c2e97ca3128289ec6944140
MyMac <mymac@mymac-no-macbook-pro.local>
2017-02-20 08:52:24 +0800

    Exercise8

---
+++ exercise8.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func FtoC(Fahrenheit:Double) -> Double{
+
+    return (Fahrenheit - 32) * 5 / 9
+}
+
+FtoC(Fahrenheit : 50)
